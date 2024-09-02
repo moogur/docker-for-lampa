a3615db3adfb1a599d50ff874450056b29f44a3f
Aleksei <a.dvoretskii@platform.live>
2024-09-02 21:43:51 +0300

    [DOC-477] feature(github action): add build application

---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+git clone --depth=1 --single-branch -b main https://github.com/yumata/lampa.git && \
+cd lampa && \
+rm -fr .git && \
+rm -f .dockerignore Dockerfile README.md SECURITY.md LICENSE
+zip -9 -r application.zip ./*
